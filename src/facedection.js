6242a9693d6ca03540f37cc761a253c1d2282481
akobepeter <mrpeterakobe@gmail.com>
2019-12-30 23:14:48 +0100

    message

--- src/facedection.js
+++ src/facedection.js
@@ -1,4 +1,4 @@
-// require('@tensorflow/tfjs-node');
+//require('@tensorflow/tfjs');
 const MODEL_PATH = './src/models';
 const faceapi = require('face-api.js');
 const canvas = require('canvas');
@@ -8,7 +8,9 @@ faceapi.env.monkeyPatch({Canvas, Image, ImageData});
 
 ///Load all the models for detecting faces
 exports.loadModels = async ()=>{
-    
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_PATH).catch((e)=>{console.log(e)});
+    await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_PATH).catch((e)=>{console.log(e)});
+    await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_PATH).catch((e)=>{console.log(e)}); 
 }
 
 
@@ -19,10 +21,9 @@ exports.loadModels = async ()=>{
 
  exports.detectFace = async () => {
 
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_PATH).catch((e)=>{console.log(e)});
      var image = await canvas.loadImage('./src/images/peter1.jpg');
      try{
-        let fullFaceDetetection = await faceapi.detectAllFaces(image);
+        let fullFaceDetetection = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
         console.log(fullFaceDetetection);
      }
      catch(err){
@@ -30,6 +31,25 @@ exports.loadModels = async ()=>{
      }
     
     // console.log(fullFaceDetetection);
+}
+
+exports.recognizeFace = async()=>{
+  
+    let names = ['kemi', 'peter'];
+
+    let labeledFaceDescriptors = await Promise.all(names.map(async (name,i)=>{
+        let descriptors = [];
+        let imagepath = `./src/faces/${name}`
+        for(let i = 1; i<5; i++){
+            let image = canvas.loadImage(imagepath + `${i}.jpg`);
+            let faceDescriptor = await faceapi.detectSingleFace(image).withFaceDescriptors().withFaceLandmarks();
+            descriptors.push(faceDescriptor.descriptor);
+        }
+
+        return new faceapi.labeledFaceDescriptors(name,descriptors);
+
+    }));
+
 }
 
 
