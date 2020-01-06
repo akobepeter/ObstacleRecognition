0da5692a8176626487f723f4bea178207d0fdcb7
unknown <victoradelowo@yahoo.com>
2020-01-06 23:41:26 +0100

    message

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 var path = require('path');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 const port = 3000;
 const facedection = require('./src/facedection');
 const {detectFace, loadModels, recognizeFace} = facedection;
@@ -8,6 +10,7 @@ const objectdetection = require('./src/objectdetection');
 const {detectObject} = objectdetection
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(fileUpload());
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/detectFace', async(req, res)=>{
@@ -24,6 +27,42 @@ app.get('/detectObject', async(req, res)=>{
     let path = `src/testimages/${imagename}.jpg`;
     await detectObject(path);
     res.send('done');
-})
+});
+app.post('/addPicture', (req, res)=>{
+    let name = req.body.name;
+    let files = [];
+    for(let i = 1; i <=5; i++){
+        let fname = "file" + i;
+        files.push(req.files[fname]);
+    }
+    let path = 'src/faces/' + name + '/';
+    if(fs.existsSync(path)){
+        files.map((file,i)=>{
+            let splitname = file.name.split('.');
+            let ext = splitname[splitname.length -1];
+            let filename = path + (i+1) + '.' + ext;
+            file.mv(filename, (err)=>{
+                if(err) res.status(500).send(err);
+            });
+        });
+    }
+    else{
+        fs.mkdirSync(path);
+        files.map((file,i)=>{
+            let splitname = file.name.split('.');
+            let ext = splitname[splitname.length -1];
+            let filename = path + (i+1) + '.' + ext;
+            file.mv(filename, (err)=>{
+                if(err) res.status(500).send(err);
+            });
+        });
+    }
+
+    res.status(200).send({
+        message: "Face Has been added",
+        status: 'success'
+    })
+
+});
 
 app.listen(port, () => console.log(`Obtacle Detection server running at ${port}`))
