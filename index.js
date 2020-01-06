802d9a716f8ca7159fea8c661677d16db9615dd7
unknown <victoradelowo@yahoo.com>
2020-01-06 22:56:53 +0100

    'message'

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const app = express()
+const app = express();
+var path = require('path');
 const port = 3000;
 const facedection = require('./src/facedection');
 const {detectFace, loadModels, recognizeFace} = facedection;
 const objectdetection = require('./src/objectdetection');
 const {detectObject} = objectdetection
 
-
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/detectFace', async(req, res)=>{
